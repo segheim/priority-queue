472698042596cd734dc66a9feff32d02c8b6927c
Evgeni Savitsky <evgeni.savitsky@rndpoint.com>
2023-10-18 12:27:57 +0300

    priority queue

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Person implements Comparable<Person>{
+
+    private final int age;
+    private final String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.age == o.age) {
+            return 0;
+        }
+        if (this.age > o.age) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/PriorityQueueNew.java
+++ src/main/java/org/example/PriorityQueueNew.java
@@ -2,21 +2,34 @@ package org.example;
 
 import java.util.AbstractQueue;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 
-public class PriorityQueueNew extends AbstractQueue {
+public class PriorityQueueNew<E> extends AbstractQueue<E> {
 
     private static final int DEFAULT_CAPACITY = 8;
 
-    Integer[] queue;
+    Object[] queue;
     int size;
+    Comparator<? super E> comparator;
 
-    PriorityQueueNew(int capacity) {
-        this.queue = new Integer[capacity];
+    public PriorityQueueNew(int initCapacity, Comparator<? super E> comparator) {
+        if (initCapacity < 1)
+            throw new IllegalArgumentException();
+        this.queue = new Object[initCapacity];
+        this.comparator = comparator;
     }
 
-    PriorityQueueNew() {
-        this.queue = new Integer[DEFAULT_CAPACITY];
+    public PriorityQueueNew(int capacity) {
+        this(capacity, null);
+    }
+
+    public PriorityQueueNew() {
+        this(DEFAULT_CAPACITY, null);
+    }
+
+    public PriorityQueueNew(Comparator<? super E> comparator) {
+        this(DEFAULT_CAPACITY, comparator);
     }
 
     @Override
@@ -29,7 +42,17 @@ public class PriorityQueueNew extends AbstractQueue {
         return 0;
     }
 
-    public boolean add(Integer element) {
+    public boolean add(E element) {
+        return offer(element);
+    }
+
+    private void growCapacity() {
+        int newCapacity = size + DEFAULT_CAPACITY;
+        queue = Arrays.copyOf(queue, newCapacity);
+    }
+
+    @Override
+    public boolean offer(E element) {
         if (element == null) {
             throw new NullPointerException();
         }
@@ -42,12 +65,20 @@ public class PriorityQueueNew extends AbstractQueue {
         if (size >= queue.length) {
             growCapacity();
         }
-        siftUp(size, element, queue);
+        siftUp(size, element);
         size = length + 1;
         return true;
     }
 
-    private void siftUp(int i, Integer element, Integer[] queue) {
+    private void siftUp(int i, E element) {
+        if (comparator != null) {
+            siftUpComparator(i, element, queue, comparator);
+        } else {
+            siftUpComparable(i, element, queue);
+        }
+    }
+
+    private <T> void siftUpComparator(int i, T element, Object[] mass, Comparator<? super T> cmp) {
         while (i > 0) {
             int k;
             if (i % 2 == 0) {
@@ -55,64 +86,111 @@ public class PriorityQueueNew extends AbstractQueue {
             } else {
                 k = i / 2;
             }
-            if (element < queue[k]) {
-                queue[i] = queue[k];
-                queue[k] = element;
+            if (cmp.compare(element, (T) mass[k]) < 0) {
+                mass[i] = mass[k];
+                mass[k] = element;
                 i = k;
             } else {
-                queue[i] = element;
+                mass[i] = element;
                 break;
             }
         }
     }
 
-    private void growCapacity() {
-        int newCapacity = size + DEFAULT_CAPACITY;
-        queue = Arrays.copyOf(queue, newCapacity);
+    private <T> void siftUpComparable(int i, T element, Object[] mass) {
+        Comparable<? super T> key = (Comparable<? super T>) element;
+        while (i > 0) {
+            int k;
+            if (i % 2 == 0) {
+                k = i / 2 - 1;
+            } else {
+                k = i / 2;
+            }
+            if (key.compareTo((T) mass[k]) < 0) {
+                mass[i] = mass[k];
+                mass[k] = element;
+                i = k;
+            } else {
+                mass[i] = element;
+                break;
+            }
+        }
     }
 
-    @Override
-    public boolean offer(Object o) {
-        return false;
-    }
+
 
     @Override
-    public Integer poll() {
+    public E poll() {
         int i = 0;
-        int firstElement = queue[i];
+        E firstElement = (E) queue[i];
         size = size - 1;
         queue[i] = queue[size];
         queue[size] = null;
+        siftDown(i);
+        return firstElement;
+    }
 
+    private void siftDown(int i) {
+        if (comparator != null) {
+            siftDownComparator(i, queue, comparator);
+        } else {
+            siftDownComparable(i, queue);
+        }
+    }
+
+    private<T> void siftDownComparable(int i, Object[] mass) {
+        Comparable<? super T> key = (Comparable<? super T>) mass[i * 2 + 1];
         while ((i * 2 + 1) < size) {
             int k;
-            if (queue[i * 2 + 2] != null) {
-                if (queue[i * 2 + 1] > queue[i * 2 + 2]) {
+            if (mass[i * 2 + 2] != null) {
+                if (key.compareTo((T) mass[i * 2 + 2]) > 0 ) {
                     k = i * 2 + 2;
-                    int temp = queue[i];
-                    queue[i] = queue[k];
-                    queue[k] = temp;
+                    swapElements(i, k, mass);
                 } else {
                     k = i * 2 + 1;
-                    int temp = queue[i];
-                    queue[i] = queue[k];
-                    queue[k] = temp;
+                    swapElements(i, k, mass);
                 }
             } else {
                 k = i * 2 + 1;
-                if (queue[k] < queue[i]) {
-                    int temp = queue[i];
-                    queue[i] = queue[k];
-                    queue[k] = temp;
+                Comparable<? super T> key1 = (Comparable<? super T>) mass[i];
+                if (key1.compareTo((T) mass[k]) > 0) {
+                    swapElements(i, k, mass);
                 }
             }
             i = k;
         }
-        return firstElement;
+    }
+
+
+    private <T> void siftDownComparator(int i, Object[] mass, Comparator<? super T> cmp) {
+        while ((i * 2 + 1) < size) {
+            int k;
+            if (mass[i * 2 + 2] != null) {
+                if (cmp.compare((T) mass[i * 2 + 1], (T) mass[i * 2 + 2]) > 0) {
+                    k = i * 2 + 2;
+                    swapElements(i, k, mass);
+                } else {
+                    k = i * 2 + 1;
+                    swapElements(i, k, mass);
+                }
+            } else {
+                k = i * 2 + 1;
+                if (cmp.compare((T) mass[i], (T) mass[k]) > 0) {
+                    swapElements(i, k, mass);
+                }
+            }
+            i = k;
+        }
+    }
+
+    private <T> void swapElements(int i, int k, Object[] mass) {
+        T temp = (T) mass[i];
+        mass[i] = mass[k];
+        mass[k] = temp;
     }
 
     @Override
-    public Object peek() {
-        return queue[0];
+    public E peek() {
+        return (E) queue[0];
     }
 }
